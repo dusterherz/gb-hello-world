9bd56d8afbbc0209bc966535ee6953d20cc759ad
Aude Lejuez <dusterherz@gmail.com>
2021-03-12 17:22:40 +0100

    handle multiples sprites movement

--- sprite/main.asm
+++ sprite/main.asm
@@ -15,7 +15,7 @@ SECTION "Game Code", rom0
 
 Start:
 .init
-    ld a, [rLY]
+    ld a, [rLY] 
     cp a, 144
     jr c, .init
 
@@ -45,17 +45,26 @@ Start:
     ld a, %11100100
     ld [rOBP1], a
 
-.setSprite:
-    ld a, 15
-    ld [OBJ0_X], a
+.setSprites:
+    ld a, 16
+    ld [rOBJ0_X], a
+    ld a, 150
+    ld [rOBJ1_X], a
     ld a, 8
-    ld [OBJ0_Y], a
+    ld [rOBJ0_Y], a
+    ld a, 160
+    ld [rOBJ1_Y], a
     ld a, MOV_RIGHT
-    ld [OBJ0_DIR], a
-    ld a, 2
-    ld [OBJ0_CHR], a
+    ld [rOBJ0_DIR], a
+    ld a, MOV_LEFT
+    ld [rOBJ1_DIR], a
+    ld a, 3
+    ld [rOBJ0_CHR], a
+    ld [rOBJ1_CHR], a
     ld a, OAMF_PAL1 | OAMF_PRI
-    ld [OBJ_ATTR], a
+    ld [rOBJ0_ATTR], a
+    ld a, OAMF_PAL1 | OAMF_PRI | OAMF_XFLIP
+    ld [rOBJ1_ATTR], a
 
 .enableScreen
     ld a, LCDCF_ON|LCDCF_BG9800|LCDCF_OBJON|LCDCF_OBJ8|LCDCF_BGON
@@ -71,40 +80,76 @@ Main:
     jr z, Main
     xor a
     ld [rIF], a ; wait for Vblank in a clean way
-    call MoveSprite
+    call MoveSprites
     jr Main
 MainEnd:
 
-MoveSprite:
-    ld a, [OBJ0_DIR]
-    and a, MOV_LEFT
-    jp nz, .moveLeft
-    ld a, [OBJ0_DIR]
-    and a, MOV_RIGHT
-    jp nz, .moveRight
-.moveLeft
-    ld hl, OBJ0_Y
-    dec [hl]
-    jr .checkDirection
-.moveRight
-    ld hl, OBJ0_Y
-    inc [hl]
-    jr .checkDirection
-.checkDirection
-    ld a, [OBJ0_Y]
-    cp a, 50
-    jp z, .directionLeft
-    cp a, 0
-    jp z, .directionRight
-    ret
-.directionLeft
-    ld a, MOV_LEFT
-    ld [OBJ0_DIR], a
-    ret
-.directionRight
-    ld a, MOV_RIGHT
-    ld [OBJ0_DIR], a
-    ret
+MoveSprites:
+    ld d, OBJS_SIZE
+    ld hl, rOBJ0_X
+.moveSpritesLoop
+    ld a, d
+    or a
+    ret z ; if d = 0, moveSprites is finished
+    .moveSprite:
+        push hl
+        ld bc, $180 ; difference between rOBJ0_X and rOBJ0_DIR
+        add hl, bc
+        ld a, [hl]
+        and a, MOV_LEFT
+        jp nz, .moveLeft
+        ld a, [hl]
+        and a, MOV_RIGHT
+        jp nz, .moveRight
+    .moveLeft
+        pop hl
+        inc hl
+        dec [hl]
+        jr .checkDirection
+    .moveRight
+        pop hl
+        inc hl
+        inc [hl]
+        jr .checkDirection
+    .checkDirection
+        ld a, [hl]
+        dec hl
+        push hl
+        cp a, 160
+        jp nc, .directionLeft
+        cp a, 8
+        jp c, .directionRight
+        jr .moveSpritesLoopEnd
+    .directionLeft
+        ld bc, $180 ; difference between rOBJ0_Y and rOBJ0_DIR
+        add hl, bc 
+        ld a, MOV_LEFT
+        ld [hl], a
+        pop hl
+        push hl
+        ld bc, 3
+        add hl, bc
+        ld a,  OAMF_PAL1 | OAMF_PRI | OAMF_XFLIP
+        ld [hl], a
+        jr .moveSpritesLoopEnd
+    .directionRight
+        ld bc, $180 ; difference between rOBJ0_Y and rOBJ0_DIR
+        add hl, bc 
+        ld a, MOV_RIGHT
+        ld [hl], a
+        pop hl
+        push hl
+        ld bc, 3
+        add hl, bc
+        ld a,  OAMF_PAL1 | OAMF_PRI
+        ld [hl], a
+        jr .moveSpritesLoopEnd
+    .moveSpritesLoopEnd
+        pop hl
+        ld bc, 4
+        add hl, bc
+        dec d
+        jr .moveSpritesLoop
 
 SECTION "Tiles", ROM0
 
